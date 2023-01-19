6bfb49363e78ede2b2bc817335f3a8ea44d56ad6
Chohanbin0511 <hanbin159@naver.com>
2023-01-19 16:48:41 +0900

    안쓰는 코드 제거

--- src/plugins/vite-plugin-fhir.js
+++ src/plugins/vite-plugin-fhir.js
@@ -5,22 +5,6 @@ export default function createFhir() {
 	const virtualModuleId = 'virtual:fhir-module';
 	const resolvedVirtualModuleId = '\0' + virtualModuleId;
 
-	// return {
-	// 	name: 'my-example', // this name will show up in warnings and errors
-	// 	resolveId(source) {
-	// 		if (source === 'virtual-module') {
-	// 			return source; // this signals that rollup should not ask other plugins or check the file system to find this id
-	// 		}
-	// 		return null; // other ids should be handled as usually
-	// 	},
-	// 	load(id) {
-	// 		if (id === 'virtual-module') {
-	// 			return 'export default "This is virtual!"'; // the source code for "virtual-module"
-	// 		}
-	// 		return null; // other ids should be handled as usually
-	// 	},
-	// };
-
 	return {
 		name: 'create-fhir',
 		resolveId(id) {
@@ -30,9 +14,6 @@ export default function createFhir() {
 		},
 		load(id) {
 			if (id === resolvedVirtualModuleId) {
-				// return fhir();
-				// return `export const msg = ${fhir}`;
-				// return `${fhir}`;
 				return `const msg = 'test'`;
 			}
 		},
